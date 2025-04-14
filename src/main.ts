69f4d25494fdedb8365112f838986dad14890925
TienNguyen <tienvannguyen.dev@gmail.com>
2025-04-14 15:09:25 +0700

    fix: global pipes error

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
-import { ValidationPipe } from '@nestjs/common';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ValidationError } from 'class-validator';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: (validationErrors: ValidationError[] = []) => {
+        return new BadRequestException(
+          validationErrors.map((error) => ({
+            [error.property]: Object.values(error.constraints || {})[0],
+          })),
+        );
+      },
+    }),
+  );
   await app.listen(3000);
 }
+
 bootstrap();
